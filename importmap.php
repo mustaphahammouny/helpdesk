4c89cfdc22a57afe432395f62d24ad05e9ff1184
Mustapha <mustapha.hammouny@gmail.com>
2026-09-22 19:35:54 +0100

    feat: setup tabler

--- importmap.php
+++ importmap.php
@@ -27,4 +27,6 @@ return [
     '@hotwired/stimulus' => ['version' => '3.2.2'],
     '@symfony/stimulus-bundle' => ['path' => './vendor/symfony/stimulus-bundle/assets/dist/loader.js'],
     '@hotwired/turbo' => ['version' => '8.0.23'],
+    '@tabler/core' => ['version' => '1.5.1'],
+    '@tabler/core/dist/css/tabler.min.css' => ['version' => '1.5.1', 'type' => 'css'],
 ];
